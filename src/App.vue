72b122240b979ad198a23c3482d6ccca86679585
Deni Setiawan <deni.setiawan@live.com>
2021-02-09 21:24:43 +0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <h1>PetBook</h1>
+    <img alt="Image 1" src="https://images.unsplash.com/photo-1517213849290-bbbfffdc6da3">
+    <img alt="Image 2" src="https://images.unsplash.com/photo-1520315342629-6ea920342047">
+    <img alt="Image 3" src="https://images.unsplash.com/photo-1505776777247-d26acc0e505b">
+    <img alt="Image 4" src="https://images.unsplash.com/photo-1548907084-a7e5f11d229d">
+    <img alt="Image 5" src="https://images.unsplash.com/photo-1596854307809-6e754c522f95">
+    <img alt="Image 6" src="https://images.unsplash.com/photo-1572478465144-f5f6573e8bfd">
+    <h2>Dibuat dengan vuejs</h2>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+img {
+  width: 100%;
+}
+</style>
